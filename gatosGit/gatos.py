dfae621b02f6d0a5ed316cb6d1ba2c6ae941d8a6
marpermon <maria.perezmonge@ucr.ac.cr>
2023-01-15 19:45:09 -0600

    added gato2

--- gatosGit/gatos.py
+++ gatosGit/gatos.py
@@ -5,3 +5,9 @@ gato1.nombre = "Sussy"
 gato1.color = "negro"
 gato1.edad = 4
 gato1.aniadirlista()
+
+gato2 = funcionesTarea.gato()
+gato2.nombre = "Knight"
+gato2.color = "blanco"
+gato2.edad = 1
+gato2.aniadirlista()
